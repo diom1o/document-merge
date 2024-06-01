820001d47f6be86750e73234cd3cdffc0d2c5aeb
diom1o <161154194+diom1o@users.noreply.github.com>
2024-06-01 19:59:12 +0200

    change variable names

--- server/helpers.py
+++ server/helpers.py
@@ -6,35 +6,35 @@ from packaging import version
 
 load_dotenv()
 
-def generate_unique_identifier():
+def generate_uuid():
     return str(uuid.uuid4())
 
-def save_file(file_path, content):
+def write_content_to_file(file_path, content):
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
     with open(file_path, 'w') as file:
         file.write(content)
 
-def get_file_path(doc_id):
-    base_path = os.getenv('BASE_STORAGE_PATH', './storage')
-    return os.path.join(base_path, f'{doc_id}.txt')
+def construct_file_path(document_id):
+    base_storage_directory = os.getenv('BASE_STORAGE_PATH', './storage')
+    return os.path.join(base_storage_directory, f'{document_id}.txt')
 
-def file_exists(file_path):
+def check_file_existence(file_path):
     return os.path.exists(file_path)
 
-def delete_file(file_path):
-    if file_exists(file_path):
+def remove_file(file_path):
+    if check_file_existence(file_path):
         os.remove(file_path)
 
-def compare_versions(version1, version2):
-    v1 = version.parse(version1)
-    v2 = version.parse(version2)
-    if v1 > v2:
+def compare_two_versions(version_a, version_b):
+    parsed_version_a = version.parse(version_a)
+    parsed_version_b = version.parse(version_b)
+    if parsed_version_a > parsed_version_b:
         return 1
-    elif v1 < v2:
+    elif parsed_version_a < parsed_version_b:
         return -1
     else:
         return 0
 
-def move_file(source_path, destination_path):
-    os.makedirs(os.path.dirname(destination_path), exist_ok=True)
-    shutil.move(source_path, destination_path)
+def relocate_file(origin_path, target_path):
+    os.makedirs(os.path.dirname(target_path), exist_ok=True)
+    shutil.move(origin_path, target_path)
